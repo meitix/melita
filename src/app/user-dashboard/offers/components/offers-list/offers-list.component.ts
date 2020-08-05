115470f345d43f7f1f9ebf42059f67b96a1d38ca
Meitix <m.valizadeh@live.com>
2020-08-05 21:30:46 +0800

    error handling enhanced in offers component

--- src/app/user-dashboard/offers/components/offers-list/offers-list.component.ts
+++ src/app/user-dashboard/offers/components/offers-list/offers-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OffersService } from '../../offers.service';
 import { IOffer } from '../../models';
-
+import { finalize } from 'rxjs/operators';
 @Component({
   selector: 'app-offers-list',
   templateUrl: './offers-list.component.html',
@@ -10,10 +10,12 @@ import { IOffer } from '../../models';
 export class OffersListComponent implements OnInit {
   offers: IOffer[];
   errors: string[] = [];
+  isLoading = true;
+
   constructor(private offersService: OffersService) {}
 
   ngOnInit(): void {
-    this.offersService.getOffers().subscribe(
+    this.offersService.getOffers().pipe(finalize(() => this.isLoading = false)).subscribe(
       (data: { offers: IOffer[] }) => (this.offers = data.offers),
       (error) => {
         console.error(error);
